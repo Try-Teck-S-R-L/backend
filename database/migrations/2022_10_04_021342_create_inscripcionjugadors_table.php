16f2ce70140c2a5b40d1c50906e0825d9771f977
mavl44 <maury.vargasl@gmail.com>
2022-10-03 22:17:39 -0400

    corrigiendo errores de base de datos

---
+++ database/migrations/2022_10_04_021342_create_inscripcionjugadors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inscripcionjugadors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('categoria');
+            $table->string('nombresJugador');
+            $table->string('apellidosJugador');
+            $table->string('nacionalidadJugador');
+            $table->string('tallaJugador');
+            $table->integer('nroCamisetaJugador');
+            $table->integer('edadJugador');
+            $table->String('posicionJugador');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inscripcionjugadors');
+    }
+};
