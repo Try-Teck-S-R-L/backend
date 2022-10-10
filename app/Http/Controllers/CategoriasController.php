d71a67428b65c0e5f4fb8737434e62e3a76ec8b0
nathdcg07 <nathdcgutierrez@gmail.com>
2022-10-10 19:34:50 -0400

    lista de jugadores lectura resubido

---
+++ app/Http/Controllers/CategoriasController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categorias;
+
+class CategoriasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categorias=Categorias::all(); //trae todos los registros
+        return $categorias;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $categorias=new Categorias();
+        $categorias->nombreValor = $request->nombreValor;
+
+        $categorias->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $categorias = Categorias::findOrFail($request->id);
+        $categorias->nombreValor = $request->nombreValor;
+
+        $categorias->save();
+
+        return $categorias;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $categorias = Categorias::destroy($request->id);
+
+        return $categorias;
+    }
+}
---
+++ app/Models/Categorias.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categorias extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nombreValor'];
+}
