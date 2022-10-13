022ae0f58df49c6f5cfd9101f994ef10fed71e6d
nathdcg07 <nathdcgutierrez@gmail.com>
2022-10-13 18:16:36 -0400

    Corregido, más que todo la parte de la api

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,10 @@ Route::post('/jugador','App\Http\Controllers\jugadorController@store'); //crear 
 Route::put('/jugador/{id}','App\Http\Controllers\jugadorController@update'); //actualizar un registro
 Route::delete('/jugador','App\Http\Controllers\jugadorController@destroy'); //borrar un registro
 Route::get('/Categorias','App\Http\Controllers\CategoriasController@index'); //para tener todos los registros y mostrarlos
-Route::post('/Categorias','App\Http\Controllers\CategoriasController@index'); //crear un registro
+Route::post('/Categorias','App\Http\Controllers\CategoriasController@store'); //crear un registro
 Route::get('/Pais','App\Http\Controllers\paisController@index'); //para tener todos los registros y mostrarlos
-Route::post('/Pais','App\Http\Controllers\paisController@index'); //crear un registro
+Route::post('/Pais','App\Http\Controllers\paisController@store'); //crear un registro
 Route::get('/posicions','App\Http\Controllers\posicionsController@index'); //para tener todos los registros y mostrarlos
-Route::post('/posicions','App\Http\Controllers\posicionsController@index'); //crear un registro
+Route::post('/posicions','App\Http\Controllers\posicionsController@store'); //crear un registro
 Route::get('/talla','App\Http\Controllers\tallaController@index'); //para tener todos los registros y mostrarlos
-Route::post('/talla','App\Http\Controllers\tallaController@index'); //crear un registro
+Route::post('/talla','App\Http\Controllers\tallaController@store'); //crear un registro
