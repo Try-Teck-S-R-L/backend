af5d2aa0a9b1fc3e79ddf07740b6ef2098dd60a4
nathdcg07 <nathdcgutierrez@gmail.com>
2022-10-13 22:16:15 -0400

    corrigiendo talla por tallas again

--- database/migrations/2022_10_10_232844_create_tallas_table.php
+++ database/migrations/2022_10_10_232844_create_tallas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tallass', function (Blueprint $table) {
+        Schema::create('tallas', function (Blueprint $table) {
             $table->id();
             $table->string('nombreValor');
             $table->timestamps();
