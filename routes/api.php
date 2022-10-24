3b048e1cff76006a9cacac931bd0735d091169b2
Gabriel Gonzales <gabo.gonzales@gmail.com>
2022-10-24 03:59:02 -0400

    CRUD del delegado hecho

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+//rutas preincripciones
 Route::get('/preinscripciones', 'App\Http\Controllers\PreinscripcionController@index');
 Route::post('/preinscripciones', 'App\Http\Controllers\PreinscripcionController@store');
+
+//rutas jugadores
 Route::post('/jugadores', 'App\Http\Controllers\InscripcionJugadorController@store');
 
+//rutas equipos
 Route::get('/equipos', 'App\Http\Controllers\InscripcionEquipoController@index');
+
+
+//rutas delegados
+Route::get('/delegado', 'App\Http\Controllers\DelegadoController@index');
+Route::post('/delegado', 'App\Http\Controllers\DelegadoController@store');
+Route::put('/delegado/{id}', 'App\Http\Controllers\DelegadoController@update');
+Route::delete('/delegado/{id}', 'App\Http\Controllers\DelegadoController@destroy');
