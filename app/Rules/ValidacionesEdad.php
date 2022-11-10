e4008a4e181bb28e8c66d1c28d107c55a90f62bf
gabriel gonzales <gabo.gonzales1801@gmail.com>
2022-11-09 22:50:19 -0400

    validaciones completas

---
+++ app/Rules/ValidacionesEdad.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\InvokableRule;
+
+class ValidacionesEdad implements InvokableRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        $categoria = DB::table('categorias')
+                    ->select('idCategoria')
+                    ->join('equipos','equipos.idCategoria','=','categoria.idCategoria')
+                    ->where('equipos.idCategoria','=','categoria.idCategoria')
+                    ->get();
+        if($categoria = '1' && $value>'35' && $value<'45'){
+            return true;
+        }elseif($categoria = '2'&& $value='45'){
+            return true;
+        }elseif($categoria = '3' && $value>'45' && $value<'55'){
+            return true;
+        }else{
+            $fail('tu edad no pertenece a esta categoria');
+        }
+    }
+}
